0896a9de9883f47befdc413dbca06f229bb8e416
Lawrence Emenike <65117830+lawrenceemenike@users.noreply.github.com>
2025-10-06 18:23:32 +0100

    Update logger_mlflow.py

--- app/logger_mlflow.py
+++ app/logger_mlflow.py
@@ -1,1 +1,12 @@
+import mlflow
+import os, time
 
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000"))
+mlflow.set_experiment("simple-llmops-chatbot")
+
+def log_interaction(model_name, prompt, response):
+    with mlflow.start_run(run_name=model_name):
+        mlflow.log_param("model", model_name)
+        mlflow.log_param("timestamp", time.strftime("%Y-%m-%d %H:%M:%S"))
+        mlflow.log_text(prompt, "prompt.txt")
+        mlflow.log_text(response, "response.txt")
